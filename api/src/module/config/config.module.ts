5a9de34b7e0e9c14d3f815b2b8e3b47e13fd22d8
devnok <ha819ha.dev@gmail.com>
2024-11-30 23:03:25 +0900

    feat: Place API

--- api/src/module/config/config.module.ts
+++ api/src/module/config/config.module.ts
@@ -1,18 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, DotenvSourceProvider } from '@nestjs-library/config';
 
-import { NodeEnv } from 'src/constant/node-env.enum';
 import path from 'path';
 import { TypeORMConfigService } from 'src/module/config/typeorm-config.service';
 import { EnvConfigService } from 'src/module/config/env-config.service';
 import { JwtConfigService } from 'src/module/config/jwt-config.service';
 import { FirebaseConfigService } from './firebase-config.service';
 
-export const getEnvFilePath = () =>
-  path.join(
-    process.cwd(),
-    `.env.${(process.env.NODE_ENV === NodeEnv.LOCAL ? NodeEnv.DEVELOPMENT : process.env.NODE_ENV) ?? NodeEnv.LOCAL}`,
-  );
+export const getEnvFilePath = () => path.join(process.cwd(), `.env`);
 
 @Global()
 @Module({
--- api/src/module/stream/stream.service.ts
+++ api/src/module/stream/stream.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Stream } from 'src/module/stream/entities/stream.entity';
@@ -16,5 +16,18 @@ export class StreamService {
 
   async findOne(streamKey: string) {
     return await this.streamRepository.findOne({ where: { streamKey } });
+  }
+
+  async getStreamURL(streamKey: string) {
+    const stream = await this.streamRepository.findOne({
+      where: { streamKey },
+      relations: ['user'],
+    });
+
+    if (!stream) {
+      throw new HttpException('Stream not found', HttpStatus.NOT_FOUND);
+    }
+
+    return true;
   }
 }
